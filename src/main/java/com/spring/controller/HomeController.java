0482e6bde4d62a47bd861e6d46e079a97b0f1c9a
HanJung <rndudb@naver.com>
2021-07-13 20:01:27 +0900

    zz

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -9,49 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.spring.service.TestService;
-
-import lombok.extern.log4j.Log4j2;
-
-/**
- * Handles requests for the application home page.
- */
-@Controller
-@Log4j2
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	
-	@Autowired
-	private TestService service;
-	
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		log.info("Å×½ºÆ®");
-		service.testSelect();
-		
-		
-		
-		return "/mypage";
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -73,7 +30,6 @@ public class HomeController {
 		
 		
 		return "/index";
->>>>>>> refs/heads/hanjung
 	}
 	
 }
