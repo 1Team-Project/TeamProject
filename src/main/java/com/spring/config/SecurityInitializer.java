fc3a8bc995cbce67fccb2b3e87d3e1460e199f72
youngkwonnoh <yknoh.sol@gmail.com>
2021-07-20 16:53:53 +0900

    21.7.20 새 브랜치 커밋 테스트

---
+++ src/main/java/com/spring/config/SecurityInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+	/* web.xml <!-- security filter -->
+	 * <filter> <filter-name>springSecurityFilterChain</filter-name>
+	 * <filter-class>org.springframework.web.filter.DelegatingFilterProxy</filter-
+	 * class> </filter> <filter-mapping>
+	 * <filter-name>springSecurityFilterChain</filter-name>
+	 * <url-pattern>/*</url-pattern> </filter-mapping>
+	 */
+}
